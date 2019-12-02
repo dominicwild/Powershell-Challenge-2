16ef79059aa61e87b9b11a34bf9dd2132a9dd5b9
Wild <dwild8@dxc.com>
2019-12-02 12:09:22 +0000

    Initial Commit

---
+++ SecondScript.ps1
@@ -0,0 +1,9 @@
+﻿function PrintToFile {
+ param( [String]$text )
+ $rand = Get-Random
+ Set-Content -Path 'C:\Users\dwild8\OneDrive - DXC Production\Documents\Powershell\Challenges\2\log.log' -Value $text
+}
+
+
+PrintToFile 'SomeText'
+PrintToFile 'SomeText2'
