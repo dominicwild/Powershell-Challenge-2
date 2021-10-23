9d929e85d57dccf9828fdabf10ca4c40c6c009e4
dominicwild <dominic55555@hotmail.com>
2021-10-23 23:27:01 +0100

    Generalise path

--- SecondScript.ps1
+++ SecondScript.ps1
@@ -1,7 +1,7 @@
 ﻿function PrintToFile {
  param( [String]$text )
  $rand = Get-Random
- Set-Content -Path 'C:\Users\dwild8\OneDrive - DXC Production\Documents\Powershell\Challenges\2\log.log' -Value $text
+ Set-Content -Path "$PSScriptRoot\log.log" -Value $text
 }
 
 
